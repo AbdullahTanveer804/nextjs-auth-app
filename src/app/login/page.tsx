652ba4ff0cb7da238e9b9002e008fe3d9bdcd263
AbdullahTanveer804 <abdullahdev804@gmail.com>
2025-04-19 17:26:29 +0500

    Envirnment & Folder Sutructure Setup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-center text-3xl'>Login Page</div>
+  )
+}
+
+export default page
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-center text-3xl'>SignUp Page</div>
+  )
+}
+
+export default page
